99f0249f7ffee27e58e27505e83b8e8ea891c6af
Kamadarada <felipekamada2@gmail.com>
2022-12-11 11:22:51 -0300

    Teste

---
+++ main.js
@@ -0,0 +1,28 @@
+function tocasom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+
+// para
+for(let contador = 0 ;contador < ListaDeTeclas.length; contador ++) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    //console.log(idAudio);
+    
+    tecla.onclick = function(){
+        tocasom(idAudio);
+    }
+
+    console.log(contador);
+
+}
+
