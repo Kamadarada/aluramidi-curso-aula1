9457eeb9b1c87dee7ebca5bd0a6052ff2f459b63
Kamadarada <felipekamada2@gmail.com>
2022-12-19 22:39:04 -0300

    melhorando codigo

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-function tocasom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocasom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if( elemento === null){
+    alert('elemento não encontrado');
+    }
+
+    if(elemento != null) {
+
+        if(elemento.localName === 'audio'){
+            elemento.play();
+
+        }
+    }
+
+
 }
 
 
@@ -20,6 +34,18 @@ for(let contador = 0 ;contador < ListaDeTeclas.length; contador ++) {
     
     tecla.onclick = function(){
         tocasom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento){
+        console.log(evento.code === 'Enter' || evento.code === 'Space');
+        if( evento.code == "Enter" || evento.code == "Space"){
+        tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 
     console.log(contador);
